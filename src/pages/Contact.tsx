841a2251e2e5f45a1b59c6f0312d527cfb73ffeb
hari mohan <harimohan@haris-MacBook-Air-2.local>
2025-07-04 19:47:41 +0530

    added link

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -15,6 +15,8 @@ const Contact = () => {
           <li><strong>Email:</strong> <a href="mailto:harifrontendengineer@gmail.com">harifrontendengineer@gmail.com</a></li>
           <li><strong>GitHub:</strong> <a href="https://github.com/harimohan1990" target="_blank" rel="noopener noreferrer">github.com/harimohan1990</a></li>
           <li><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/hari-mohan-prajapat-47299b54" target="_blank" rel="noopener noreferrer">linkedin.com/in/hari-mohan-prajapat</a></li>
+          <li><strong>BLOG:</strong> <a href="https://codewithhari.dev/" target="_blank" rel="noopener noreferrer">Tech Blog</a></li>
+
           <li><strong>Phone:</strong> <a href="tel:+916366653022">+91 63666 53022</a></li>
         </ul>
 
